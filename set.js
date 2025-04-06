7bfdf5405d872970a86c7ae9982ee0237885b9d9
YOUNGT456 <matonatlhompho@gmail.com>
2025-04-06 13:21:52 +0200

    Update set.js

--- set.js
+++ set.js
@@ -15,33 +15,33 @@ global.giftedRepo = "https://github.com/mauricegift/gifted-md";
 global.giftedApiRepo = "https://api.github.com/repos/mauricegift/gifted-md";
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SESSION_ID: process.env.SESSION_ID || "Gifted~fJxFQRbB#-6rNEYU3xRP9nVTfpYRLGJTtBfwX2Tso-eM_Y4081Sg", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
 SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 ANTI_DELETE: process.env.ANTI_DELETE || false, // make true to use antidelete 
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
 AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💜,💛,❤️,🤍,🖤,💙", //Input Yours Custom...Can be One Emoji or Multiple Emojis
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
-STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by Gifted-Md!", // // Input Yours custom...
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by YOUNG T-Md!", // // Input Yours custom...
 MODE: process.env.MODE || "private", // Put private or public or inbox or groups
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254715206562", // Only 1 owner Number Here, others Add to sudo numbers...
-OWNER_NAME: process.env.OWNER_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Input Yours custom...(Maintain font for Flow)
-PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
-PACK_NAME: process.env.PACK_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃💜", // Added // Input Yours custom...
-PREFIX: process.env.PREFIX || ".",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "27722247464", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NAME: process.env.OWNER_NAME || "𝐘𝐎𝐔𝐍𝐆 𝐓𝐄𝐄 𝐓𝐄𝐂𝐇", // Input Yours custom...(Maintain font for Flow)
+PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐘𝐎𝐔𝐍𝐆 𝐓𝐄𝐄 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
+PACK_NAME: process.env.PACK_NAME || "𝐘𝐎𝐔𝐍𝐆 𝐓-𝐌𝐃💜", // Added // Input Yours custom...
+PREFIX: process.env.PREFIX || "!",
 VERSION: process.env.VERSION || "5.0.0",
-ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTILINK: process.env.ANTILINK || "true", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "false",
 ANTIBAD: process.env.ANTIBAD || "false",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
-BOT_PIC: process.env.BOT_PIC || "https://files.giftedtech.web.id/file/gifted-md.jpg", // You can Replace with yours...
-AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
+BOT_NAME: process.env.BOT_NAME || "𝐘𝐎𝐔𝐍𝐆 𝐓-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
+BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/izd8l0.jpeg // You can Replace with yours...
+AUTO_AUDIO: process.env.AUTO_AUDIO || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Gifted creates things that creates other things",
-CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
+CHAT_BOT: process.env.CHAT_BOT || "true", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
